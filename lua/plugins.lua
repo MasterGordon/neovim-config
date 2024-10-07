8155a16a2343e76e9aeefc86ff7f51d04c39e5ea
MasterGordon <gologo.x@gmail.com>
2024-10-07 15:21:59 +0200

    added markview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -110,6 +110,23 @@ require("lazy").setup(
       } -- your configuration
     },
     {
+      "OXY2DEV/markview.nvim",
+      lazy = false, -- Recommended
+      -- ft = "markdown" -- If you decide to lazy-load anyway
+
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+      },
+      config = function()
+        require("markview").setup(
+          {
+            hybrid_modes = {"n"}
+          }
+        )
+      end
+    },
+    {
       "nvim-telescope/telescope.nvim",
       config = function()
         require("plugins/telescope")
